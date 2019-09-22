7f6e5dd41b0e1ae3b9f19b648736c43ac66e7910
liqiduan <lee.nju@live.com>
2019-09-22 23:36:24 +0800

    Support random place player

---
+++ human.py
@@ -0,0 +1,13 @@
+from cocos.text import Label
+from cocos.sprite import Sprite
+
+class Human(Sprite):
+    def __init__(self, name="Hero"):
+        self.name = name
+        Sprite.__init__(self, "man.gif")
+
+        self.nameLabel = Label(name, 
+                anchor_x='center', anchor_y="bottom")
+        self.nameLabel.position = (0, self.height/2)
+        self.add(self.nameLabel)
+
