c6054c7cc2e2001710bec244076391125af052ed
liqiduan <lee.nju@live.com>
2019-09-22 22:41:29 +0800

    Support map

---
+++ map.py
@@ -0,0 +1,5 @@
+from cocos.layer import Layer
+from cocos.tiles import HexMapLayer
+
+class MapLayer(HexMapLayer):
+    def __init__(self):
---
+++ tiles.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fantasyhextiles_v3_borderless" tilewidth="32" tileheight="48" tilecount="48" columns="8">
+ <image source="../game source/fantasyhextiles_v3.png" width="256" height="288"/>
+ <tile id="0" type="land">
+  <properties>
+   <property name="type" value="land"/>
+  </properties>
+  <objectgroup draworder="index" name="land">
+   <properties>
+    <property name="type" value="land"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" name="forest"/>
+ </tile>
+ <tile id="5" type="moutain">
+  <properties>
+   <property name="type" value="moutain"/>
+  </properties>
+  <objectgroup draworder="index" name="moutain">
+   <properties>
+    <property name="type" value="moutain"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="6" type="water">
+  <properties>
+   <property name="type" value="water"/>
+  </properties>
+  <objectgroup draworder="index" name="water">
+   <properties>
+    <property name="type" value="water"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" name="sea"/>
+ </tile>
+ <tile id="8" type="city">
+  <properties>
+   <property name="type" value="city"/>
+  </properties>
+  <objectgroup draworder="index" name="city">
+   <properties>
+    <property name="type" value="city"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" name="grass"/>
+ </tile>
+</tileset>
